6c0282a227f8757981e0bfb379c384fce42f18e7
KOREAIT <KOREAIT@DESKTOP-7URGO09>
2021-10-26 14:54:46 +0900

    tt

--- MiniTeam/src/miniteam/Gugu_team.java
+++ MiniTeam/src/miniteam/Gugu_team.java
@@ -2,26 +2,28 @@ package miniteam;
 
 public class Gugu_team {
 
-	public static void main(String[] args) {
-		
-		for (int i = 1; i <= 9; i++) {
-			for (int j = 2; j <= 9; j++) {
-				System.out.print(j + "*" + i + "=" + j * i + "\t");
-			}
-			System.out.println();
-	    }
-		
-		int[] arr = {1,2,3,4,5};
-		int sum = 0;
-		for (int i = 0; i < arr.length; i++) {
-			sum += arr[i];
-			System.out.println(sum);
-		}
-		for (int i = 2; i <= 9; i++) {
-			for (int j = 1; j <= 9; j++) {
-				System.out.print(i + "*" + j + "=" + j * i + "\t");
-			}
-			System.out.println();
-	    }
+   public static void main(String[] args) {
+      // TODO Auto-generated method stub
+      
+      for (int i = 1; i <= 9; i++) {
+         for (int j = 2; j <= 9; j++) {
+            System.out.print(j + "*" + i + "=" + j * i + "\t");
+         }
+         System.out.println();
+       }
+      
+      int[] arr = {1,2,3,4,5};
+      int sum = 0;
+      for (int i = 0; i < arr.length; i++) {
+         sum += arr[i];
+         System.out.println(sum);
+      }
+      for (int i = 2; i <= 9; i++) {
+         for (int j = 1; j <= 9; j++) {
+            System.out.print(i + "*" + j + "=" + j * i + "\t");
+         }
+         System.out.println();
+   }
+      //1ºü ¼ö¿¬
 
 }
